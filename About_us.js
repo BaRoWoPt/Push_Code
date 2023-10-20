d66f2df91926fb6b832460f6e137b5691d121321
Tuna004 <145894894+Tuna004@users.noreply.github.com>
2023-10-20 22:11:41 +0700

    Unfinished

--- About_us.js
+++ About_us.js
@@ -4,7 +4,6 @@ const sidebar = document.querySelector(".sidebar");
 const navsidebar = document.getElementById("navsidebar");
 const openSideBar = document.getElementById("icon_open_sidebar");
 const openNavSideBar = document.getElementById("icon_chart");
-const container_show = document.querySelector(".show_container")
 
 // // process close
 function ShowSideBar() {
@@ -24,16 +23,12 @@ function HiddenSideBar() {
 }
 closeSideBar.addEventListener("click", HiddenSideBar);
 
-container_show.addEventListener("click", function (event) {
-  sidebar.classList.remove("open");
-});
+
 
 function HiddenNavSideBar() {
   navsidebar.classList.remove("open");
 }
 closeNavBar.addEventListener("click", HiddenNavSideBar);
 
-container_show.addEventListener("click", function (event) {
-  navsidebar.classList.remove("open");
-});
+
 // //////////////////////////////////////////////////////////////////////////////////////////
