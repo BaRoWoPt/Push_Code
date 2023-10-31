67572e63c1e463a4be348e248c1445f0a8317ca9
Dao Gia Bao <dgbao.cntt@gmail.com>
2023-11-01 01:41:39 +0700

    done_responsive
    gia bao done at 01h41

--- Ticket_queue.js
+++ Ticket_queue.js
@@ -4,6 +4,7 @@ const sidebar = document.querySelector(".sidebar");
 const navsidebar = document.getElementById("navsidebar");
 const openSideBar = document.getElementById("icon_open_sidebar");
 const openNavSideBar = document.getElementById("icon_chart");
+const container_show = document.querySelector(".main-container");
 
 // // process close
 function ShowSideBar() {
@@ -22,26 +23,26 @@ function HiddenSideBar() {
   sidebar.classList.remove("open");
 }
 closeSideBar.addEventListener("click", HiddenSideBar);
-
-
-
+container_show.addEventListener("click", function (event) {
+  sidebar.classList.remove("open");
+});
 function HiddenNavSideBar() {
   navsidebar.classList.remove("open");
 }
 closeNavBar.addEventListener("click", HiddenNavSideBar);
-
-
+container_show.addEventListener("click", function (event) {
+  navsidebar.classList.remove("open");
+});
 // //////////////////////////////////////////////////////////////////////////////////////////
 
 // header hidden
 const header = document.querySelector(".header");
-			let lastScrollY = window.scrollY;
-			window.addEventListener("scroll", () => {
-				if (lastScrollY < window.scrollY) {
-					header.classList.add("header--hidden");
-				} else {
-					header.classList.remove("header--hidden");
-				}
-				lastScrollY = window.scrollY;
-			});	
-
+let lastScrollY = window.scrollY;
+window.addEventListener("scroll", () => {
+  if (lastScrollY < window.scrollY) {
+    header.classList.add("header--hidden");
+  } else {
+    header.classList.remove("header--hidden");
+  }
+  lastScrollY = window.scrollY;
+});
