34431a7d8639668db6193aed2f9998277434631a
Dao Gia Bao <dgbao.cntt@gmail.com>
2023-11-05 14:32:23 +0700

    commit

--- Event_shop.js
+++ Event_shop.js
@@ -23,8 +23,6 @@ function HiddenSideBar() {
 }
 closeSideBar.addEventListener("click", HiddenSideBar);
 
-
-
 function HiddenNavSideBar() {
   navsidebar.classList.remove("open");
 }
@@ -32,242 +30,255 @@ closeNavBar.addEventListener("click", HiddenNavSideBar);
 
 // //////////////////////////////////////////////////////////////////////////////////////////
 const header = document.querySelector(".header");
-			let lastScrollY = window.scrollY;
-			window.addEventListener("scroll", () => {
-				if (lastScrollY < window.scrollY) {
-					header.classList.add("header--hidden");
-				} else {
-					header.classList.remove("header--hidden");
-				}
-				lastScrollY = window.scrollY;
-			});	
+let lastScrollY = window.scrollY;
+window.addEventListener("scroll", () => {
+  if (lastScrollY < window.scrollY) {
+    header.classList.add("header--hidden");
+  } else {
+    header.classList.remove("header--hidden");
+  }
+  lastScrollY = window.scrollY;
+});
+
 var thongtinKH = new Array();
-function themvaogiohang(event){
-	event.preventDefault();
-	var form = document.getElementById("form_1");
-	var name = document.getElementById("fullname").value;
-	var phoneNumber = document.getElementById("thongtin").value;
-	var email = document.getElementById("gmail").value;
-	var ticketQuantity = document.getElementById("buy_ticket").value;
-	var khInput = new Array(name,phoneNumber,email,ticketQuantity);
-	if (name === "" || phoneNumber === "" || email === "" || ticketQuantity === "")
-	{
-		alert("Vui lòng nhập đủ thông tin giúp bọn mình nhé!");
-		return false;
-	}
-    function getStoredThongTinKH() {
-	var gh = sessionStorage.getItem("thongtinKH");
-	return JSON.parse(gh) || [];
+function themvaogiohang(event) {
+  event.preventDefault();
+  var form = document.getElementById("form_1");
+  var name = document.getElementById("fullname").value;
+  var phoneNumber = document.getElementById("thongtin").value;
+  var email = document.getElementById("gmail").value;
+  var ticketQuantity = document.getElementById("buy_ticket").value;
+  var khInput = new Array(name, phoneNumber, email, ticketQuantity);
+  if (
+    name === "" ||
+    phoneNumber === "" ||
+    email === "" ||
+    ticketQuantity === ""
+  ) {
+    alert("Vui lòng nhập đủ thông tin giúp bọn mình nhé!");
+    return false;
+  }
+
+  function getStoredThongTinKH() {
+    var gh = sessionStorage.getItem("thongtinKH");
+    return JSON.parse(gh) || [];
+  }
+
+  thongtinKH = getStoredThongTinKH();
+  var foundIndex = -1;
+  for (var i = 0; i < thongtinKH.length; i++) {
+    if (
+      thongtinKH[i][0] === name &&
+      thongtinKH[i][1] === phoneNumber &&
+      thongtinKH[i][2] === email
+    ) {
+      foundIndex = i;
+      break;
     }
-	thongtinKH = getStoredThongTinKH();
-    var foundIndex = -1;
-	for (var i = 0; i < thongtinKH.length; i++) {
-	if (thongtinKH[i][0] === name && thongtinKH[i][1] === phoneNumber && thongtinKH[i][2] === email) {
-	foundIndex = i;
-	 break;
-	}
-    }
-	if (foundIndex !== -1) {
-	// Nếu thông tin khách hàng đã tồn tại, cộng số lượng đặt hàng mới với số lượng đặt hàng trước đó
-	var previousQuantity = parseInt(thongtinKH[foundIndex][3], 10);
-	var newQuantity = parseInt(ticketQuantity, 10);
-	thongtinKH[foundIndex][3] = previousQuantity + newQuantity;
-    } else {
-	// Nếu thông tin khách hàng chưa tồn tại, thêm thông tin mới vào mảng
-	thongtinKH.push(khInput);
-	 }
-	sessionStorage.setItem("thongtinKH",JSON.stringify(thongtinKH));
-	console.log(thongtinKH);
-	form.reset();
-	window.scrollTo(0,0);
-	alert("Vui lòng kiểm tra trong giỏ hàng để xác nhận thông tin nhé");
-	return true;
+  }
+  if (foundIndex !== -1) {
+    var previousQuantity = parseInt(thongtinKH[foundIndex][3], 10);
+    var newQuantity = parseInt(ticketQuantity, 10);
+    thongtinKH[foundIndex][3] = previousQuantity + newQuantity;
+  } else {
+    thongtinKH.push(khInput);
+  }
+  sessionStorage.setItem("thongtinKH", JSON.stringify(thongtinKH));
+  console.log(thongtinKH);
+  form.reset();
+  window.scrollTo(0, 0);
+  alert("Vui lòng kiểm tra trong giỏ hàng để xác nhận thông tin nhé");
+  return true;
 }
+
 function showgiohang() {
-	var gh = sessionStorage.getItem("thongtinKH");
-	var giohang = JSON.parse(gh);
-	var cartBody = document.getElementById("cart-body");
-	var totalPriceCell = document.getElementById("total-price");
-	var totalPrice = 0;
-  
-	giohang.forEach(function (item, index) {
-	  var row = document.createElement("tr");
-  
-	  var nameCell = document.createElement("td");
-	  var nameSpan = document.createElement("span");
-	  nameSpan.textContent = item[0];
-	  nameCell.appendChild(nameSpan);
-	  row.appendChild(nameCell);
-  
-	  var phoneCell = document.createElement("td");
-	  var phoneSpan = document.createElement("span");
-	  phoneSpan.textContent = item[1];
-	  phoneCell.appendChild(phoneSpan);
-	  row.appendChild(phoneCell);
-  
-	  var emailCell = document.createElement("td");
-	  var emailSpan = document.createElement("span");
-	  emailSpan.textContent = item[2];
-	  emailCell.appendChild(emailSpan);
-	  row.appendChild(emailCell);
-  
-	  var quantityCell = document.createElement("td");
-	  var quantitySpan = document.createElement("span");
-	  quantitySpan.textContent = item[3];
-	  quantityCell.appendChild(quantitySpan);
-	  row.appendChild(quantityCell);
-  
-	  var priceCell = document.createElement("td");
-	  var priceSpan = document.createElement("span");
-	  var price = calculatePrice(item[3]);
-	  priceSpan.textContent = price;
-	  priceCell.appendChild(priceSpan);
-	  row.appendChild(priceCell);
-  
-	  var deleteCell = document.createElement("td");
-	  var deleteButton = document.createElement("button");
-	  deleteButton.classList.add("delete-cell");
-	  deleteButton.textContent = "Xóa";
-	  deleteButton.addEventListener("click", function () {
-		deleteCartItem(index);
-	  });
-	  deleteCell.appendChild(deleteButton);
-	  row.appendChild(deleteCell);
-  
-	  var updateCell = document.createElement("td");
-	  var updateButton = document.createElement("button");
-	  updateButton.classList.add("update-cell");
-	  updateButton.textContent = "Cập nhật";
-	  updateButton.addEventListener("click", function () {
-		enableEditMode(row, index);
-	  });
-	  updateCell.appendChild(updateButton);
-	  row.appendChild(updateCell);
-  
-	  cartBody.appendChild(row);
-  
-	  totalPrice += price;
-	});
-  
-	totalPriceCell.textContent = totalPrice;
-  
-	function calculatePrice(quantity) {
-	  var pricePerTicket = 1000000;
-	  return quantity * pricePerTicket;
-	}
-  
-	function deleteCartItem(index) {
-	  giohang.splice(index, 1);
-	  sessionStorage.setItem("thongtinKH", JSON.stringify(giohang));
-	  location.reload();
-	}
-  
-	function enableEditMode(row, index) {
-	  var cells = row.getElementsByTagName("td");
-  
-	  var nameCell = cells[0];
-	  var nameSpan = nameCell.querySelector("span");
-	  var nameText = nameSpan.textContent;
-	  var nameInput = document.createElement("input");
-	  nameInput.type = "text";
-	  nameInput.value = nameText;
-	  nameCell.replaceChild(nameInput, nameSpan);
-  
-	  var phoneCell = cells[1];
-	  var phoneSpan = phoneCell.querySelector("span");
-	  var phoneText = phoneSpan.textContent;
-	  var phoneInput = document.createElement("input");
-	  phoneInput.type = "text";
-	  phoneInput.value = phoneText;
-	  phoneCell.replaceChild(phoneInput, phoneSpan);
-  
-	  var emailCell = cells[2];
-	  var emailSpan = emailCell.querySelector("span");
-	  var emailText = emailSpan.textContent;
-	  var emailInput = document.createElement("input");
-	  emailInput.type = "text";
-	  emailInput.value = emailText;
-	  emailCell.replaceChild(emailInput, emailSpan);
-  
-	  var quantityCell = cells[3];
-	  var quantitySpan = quantityCell.querySelector("span");
-	  var quantityText = quantitySpan.textContent;
-	  var quantityInput = document.createElement("input");
-	  quantityInput.type = "text";
-	  quantityInput.value = quantityText;
-	  quantityCell.replaceChild(quantityInput, quantitySpan);
-  
-	  var updateButton = cells[6].querySelector("button");
-	  updateButton.textContent = "Lưu";
-	  updateButton.removeEventListener("click", function () {
-		enableEditMode(row, index);
-	  });
-	  updateButton.addEventListener("click", function () {
-		saveChanges(row, index);
-	  });
-	}
-  
-	function saveChanges(row, index) {
-	  var giohang = JSON.parse(sessionStorage.getItem("thongtinKH"));
-  
-	  var cells = row.getElementsByTagName("td");
-  
-	  var nameCell = cells[0];
-	  var nameInput = nameCell.querySelector("input");
-	  var nameText = nameInput.value;
-	  nameCell.removeChild(nameInput);
-	  var nameSpan = document.createElement("span");
-	  nameSpan.textContent = nameText;
-	  nameCell.appendChild(nameSpan);
-  
-	  var phoneCell = cells[1];
-	  var phoneInput = phoneCell.querySelector("input");
-	  var phoneText = phoneInput.value;
-	  phoneCell.removeChild(phoneInput);
-	  var phoneSpan = document.createElement("span");
-	  phoneSpan.textContent = phoneText;
-	  phoneCell.appendChild(phoneSpan);
-  
-	  var emailCell = cells[2];
-	  var emailInput = emailCell.querySelector("input");
-	  var emailText = emailInput.value;
-	  emailCell.removeChild(emailInput);
-	  var emailSpan = document.createElement("span");
-	  emailSpan.textContent = emailText;
-	  emailCell.appendChild(emailSpan);
-  
-	  var quantityCell = cells[3];
-	  var quantityInput = quantityCell.querySelector("input");
-	  var quantityText = quantityInput.value;
-	  quantityCell.removeChild(quantityInput);
-	  var quantitySpan = document.createElement("span");
-	  quantitySpan.textContent = quantityText;
-	  quantityCell.appendChild(quantitySpan);
+  var gh = sessionStorage.getItem("thongtinKH");
+  var giohang = JSON.parse(gh);
+  var cartBody = document.getElementById("cart-body");
+  var totalPriceCell = document.getElementById("total-price");
+  var totalPrice = 0;
 
-	  var updateButton = cells[6].querySelector("button");
-	  updateButton.textContent = "Cập nhật";
-	  updateButton.removeEventListener("click", function () {
-		saveChanges(row, index);
-	  });
-	  updateButton.addEventListener("click", function () {
-		enableEditMode(row, index);
-	  });
-  
-	  giohang[index][0] = nameText;
-	  giohang[index][1] = phoneText;
-	  giohang[index][2] = emailText;
-	  giohang[index][3] = quantityText;
+  giohang.forEach(function (item, index) {
+    var row = document.createElement("tr");
 
-	  sessionStorage.setItem("thongtinKH", JSON.stringify(giohang));
-	  location.reload();
-	  console.log(giohang);
-	}
+    var nameCell = document.createElement("td");
+    var nameSpan = document.createElement("span");
+    nameSpan.textContent = item[0];
+    nameCell.appendChild(nameSpan);
+    row.appendChild(nameCell);
+
+    var phoneCell = document.createElement("td");
+    var phoneSpan = document.createElement("span");
+    phoneSpan.textContent = item[1];
+    phoneCell.appendChild(phoneSpan);
+    row.appendChild(phoneCell);
+
+    var emailCell = document.createElement("td");
+    var emailSpan = document.createElement("span");
+    emailSpan.textContent = item[2];
+    emailCell.appendChild(emailSpan);
+    row.appendChild(emailCell);
+
+    var quantityCell = document.createElement("td");
+    var quantitySpan = document.createElement("span");
+    quantitySpan.textContent = item[3];
+    quantityCell.appendChild(quantitySpan);
+    row.appendChild(quantityCell);
+
+    var priceCell = document.createElement("td");
+    var priceSpan = document.createElement("span");
+    var price = calculatePrice(item[3]);
+    priceSpan.textContent = formatCurrency(price);
+    priceCell.appendChild(priceSpan);
+    row.appendChild(priceCell);
+
+    var deleteCell = document.createElement("td");
+    var deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-cell");
+    deleteButton.textContent = "Xóa";
+    deleteButton.addEventListener("click", function () {
+      deleteCartItem(index);
+    });
+    deleteCell.appendChild(deleteButton);
+    row.appendChild(deleteCell);
+
+    var updateCell = document.createElement("td");
+    var updateButton = document.createElement("button");
+    updateButton.classList.add("update-cell");
+    updateButton.textContent = "Cập nhật";
+    updateButton.addEventListener("click", function () {
+      enableEditMode(row, index);
+    });
+    updateCell.appendChild(updateButton);
+    row.appendChild(updateCell);
+
+    cartBody.appendChild(row);
+
+    totalPrice += price;
+  });
+
+  totalPriceCell.textContent = formatCurrency(totalPrice) + " VND";
+
+  function calculatePrice(quantity) {
+    var pricePerTicket = 1000000;
+    return quantity * pricePerTicket;
   }
-  
-  // Xóa hết bảng
-  function deleteAll() {
-	sessionStorage.removeItem("thongtinKH");
-	location.reload();
+
+  function deleteCartItem(index) {
+    giohang.splice(index, 1);
+    sessionStorage.setItem("thongtinKH", JSON.stringify(giohang));
+    location.reload();
   }
-  
-  showgiohang();
+
+  function enableEditMode(row, index) {
+    var cells = row.getElementsByTagName("td");
+
+    var nameCell = cells[0];
+    var nameSpan = nameCell.querySelector("span");
+    var nameText = nameSpan.textContent;
+    var nameInput = document.createElement("input");
+    nameInput.type = "text";
+    nameInput.value = nameText;
+    nameCell.replaceChild(nameInput, nameSpan);
+
+    var phoneCell = cells[1];
+    var phoneSpan = phoneCell.querySelector("span");
+    var phoneText = phoneSpan.textContent;
+    var phoneInput = document.createElement("input");
+    phoneInput.type = "text";
+    phoneInput.value = phoneText;
+    phoneCell.replaceChild(phoneInput, phoneSpan);
+
+    var emailCell = cells[2];
+    var emailSpan = emailCell.querySelector("span");
+    var emailText = emailSpan.textContent;
+    var emailInput = document.createElement("input");
+    emailInput.type = "text";
+    emailInput.value = emailText;
+    emailCell.replaceChild(emailInput, emailSpan);
+
+    var quantityCell = cells[3];
+    var quantitySpan = quantityCell.querySelector("span");
+    var quantityText = quantitySpan.textContent;
+    var quantityInput = document.createElement("input");
+    quantityInput.type = "text";
+    quantityInput.value = quantityText;
+    quantityCell.replaceChild(quantityInput, quantitySpan);
+
+    var updateButton = cells[6].querySelector("button");
+    updateButton.textContent = "Lưu";
+    updateButton.removeEventListener("click", function () {
+      enableEditMode(row, index);
+    });
+    updateButton.addEventListener("click", function () {
+      saveChanges(row, index);
+    });
+  }
+
+  function saveChanges(row, index) {
+    var giohang = JSON.parse(sessionStorage.getItem("thongtinKH"));
+
+    var cells = row.getElementsByTagName("td");
+
+    var nameCell = cells[0];
+    var nameInput = nameCell.querySelector("input");
+    var nameText = nameInput.value;
+    nameCell.removeChild(nameInput);
+    var nameSpan = document.createElement("span");
+    nameSpan.textContent = nameText;
+    nameCell.appendChild(nameSpan);
+
+    var phoneCell = cells[1];
+    var phoneInput = phoneCell.querySelector("input");
+    var phoneText = phoneInput.value;
+    phoneCell.removeChild(phoneInput);
+    var phoneSpan = document.createElement("span");
+    phoneSpan.textContent = phoneText;
+    phoneCell.appendChild(phoneSpan);
+
+    var emailCell = cells[2];
+    var emailInput = emailCell.querySelector("input");
+    var emailText = emailInput.value;
+    emailCell.removeChild(emailInput);
+    var emailSpan = document.createElement("span");
+    emailSpan.textContent = emailText;
+    emailCell.appendChild(emailSpan);
+
+    var quantityCell = cells[3];
+    var quantityInput = quantityCell.querySelector("input");
+    var quantityText = quantityInput.value;
+    quantityCell.removeChild(quantityInput);
+    var quantitySpan = document.createElement("span");
+    quantitySpan.textContent = quantityText;
+    quantityCell.appendChild(quantitySpan);
+
+    var updateButton = cells[6].querySelector("button");
+    updateButton.textContent = "Cập nhật";
+    updateButton.removeEventListener("click", function () {
+      saveChanges(row, index);
+    });
+    updateButton.addEventListener("click", function () {
+      enableEditMode(row, index);
+    });
+
+    giohang[index][0] = nameText;
+    giohang[index][1] = phoneText;
+    giohang[index][2] = emailText;
+    giohang[index][3] = quantityText;
+
+    sessionStorage.setItem("thongtinKH", JSON.stringify(giohang));
+    location.reload();
+    console.log(giohang);
+  }
+}
+
+function formatCurrency(amount) {
+  return amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function deleteAll() {
+  sessionStorage.removeItem("thongtinKH");
+  location.reload();
+}
+
+showgiohang();
