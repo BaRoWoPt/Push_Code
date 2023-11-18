3aca6ff7c5876cf8efe4e524fc2db5e8dd2455b3
Dao Gia Bao <dgbao.cntt@gmail.com>
2023-11-18 22:53:52 +0700

    check_button

--- php/Login_admin.php
+++ php/Login_admin.php
@@ -13,110 +13,112 @@
 </head>
 
 <body>
-<div class="header">
-    <a href="./Concert_B.php" class="icon">
-        <img class="icon_page" src="../img/logo_page.png" alt="">
-    </a>
-    <div class="menu-items">
-        <a href="Concert_B.php" class="items border_bottom">
-            <span id="item">Trang Chủ</span>
+    <div class="header">
+        <a href="./Concert_B.php" class="icon">
+            <img class="icon_page" src="../img/logo_page.png" alt="">
         </a>
-        <a href="./Layout_concert.php" class="items">
-            <span id="item">Sự Kiện</span>
-        </a>
-        <a href="About_us.php" class="items">
-            <span id="item">Về Chúng Tôi </span>
-        </a>
+        <div class="menu-items">
+            <a href="Concert_B.php" class="items border_bottom">
+                <span id="item">Trang Chủ</span>
+            </a>
+            <a href="./Layout_concert.php" class="items">
+                <span id="item">Sự Kiện</span>
+            </a>
+            <a href="About_us.php" class="items">
+                <span id="item">Về Chúng Tôi </span>
+            </a>
 
-    </div>
-    <div class="icon_left">
-        <a style="color: #ffffff;" href="Search_ticket.php"><i class="fa-solid fa-magnifying-glass"></i></a>
-        <a id="icon_chart" class="fa-solid fa-bag-shopping" style="color: #ffffff;"></a>
-        <a id="icon_open_sidebar" class=" icon_open_sidebar fa-solid fa-bars "></a>
-    </div>
-    <div class="sidebar">
-        <i id="icon_close_sidebar" class="icon_close_sidebar fa-solid fa-xmark"></i>
-        <main class="sidebar_items">
-            <ul class="option">
-                <li class="option"><a href="Concert_B.php">Trang Chủ</a></li>
-                <li class="option"><a href="Layout_concert.php">Sự Kiện</a></li>
-                <li class="option"><a href="About_us.php">Về Chúng Tôi</a></li>
-                <li class="option"><a href="PurchaseConfirm.php">Giỏ Hàng</a></li>
-                <li class="option"><a href="#">Quản lí</a></li>
-            </ul>
-        </main>
-        <footer class="sidebar_footer">
-            <footer class="sb_ft_sp">
-                <img class="icon_footer" src="../img/logo_concert.png" alt="">
-                <ul class="footer_nav">
-                    <li class="footer_link"><a href="https://www.facebook.com/profile.php?id=100022231063225"
-                            class="navfooter">Contact</a></li>
-                    <li class="footer_link"><a href="https://www.facebook.com/duyle215" class="navfooter">Facebook</a>
-                    </li>
-                    <li class="footer_link"><a href="https://www.instagram.com/oaboad_29/"
-                            class="navfooter">Instagram</a></li>
-                </ul>
-            </footer>
-        </footer>
-    </div>
-    <div id="navsidebar" class="navsidebar">
-        <i id="icon_close_navbar" class="icon_close_sidebar fa-solid fa-xmark"></i>
-        <div class="nav_type">
-            <h2 class="shopping_bag">Giỏ Hàng Của Bạn</h2>
         </div>
-        <footer class="footer_nav">
-            <button onclick="window.location.href='PurchaseConfirm.php' " class="check_nav">
-                Check
-            </button>
-        </footer>
+        <div class="icon_left">
+            <a style="color: #ffffff;" href="Search_ticket.php"><i class="fa-solid fa-magnifying-glass"></i></a>
+            <a id="icon_chart" class="fa-solid fa-bag-shopping" style="color: #ffffff;"></a>
+            <a id="icon_open_sidebar" class=" icon_open_sidebar fa-solid fa-bars "></a>
+        </div>
+        <div class="sidebar">
+            <i id="icon_close_sidebar" class="icon_close_sidebar fa-solid fa-xmark"></i>
+            <main class="sidebar_items">
+                <ul class="option">
+                    <li class="option"><a href="Concert_B.php">Trang Chủ</a></li>
+                    <li class="option"><a href="Layout_concert.php">Sự Kiện</a></li>
+                    <li class="option"><a href="About_us.php">Về Chúng Tôi</a></li>
+                    <li class="option"><a href="PurchaseConfirm.php">Giỏ Hàng</a></li>
+                    <li class="option"><a href="#">Quản lí</a></li>
+                </ul>
+            </main>
+            <footer class="sidebar_footer">
+                <footer class="sb_ft_sp">
+                    <img class="icon_footer" src="../img/logo_concert.png" alt="">
+                    <ul class="footer_nav">
+                        <li class="footer_link"><a href="https://www.facebook.com/profile.php?id=100022231063225"
+                                class="navfooter">Contact</a></li>
+                        <li class="footer_link"><a href="https://www.facebook.com/duyle215"
+                                class="navfooter">Facebook</a>
+                        </li>
+                        <li class="footer_link"><a href="https://www.instagram.com/oaboad_29/"
+                                class="navfooter">Instagram</a></li>
+                    </ul>
+                </footer>
+            </footer>
+        </div>
+        <div id="navsidebar" class="navsidebar">
+            <i id="icon_close_navbar" class="icon_close_sidebar fa-solid fa-xmark"></i>
+            <div class="nav_type">
+                <h2 class="shopping_bag">Giỏ Hàng Của Bạn</h2>
+            </div>
+            <footer class="footer_nav">
+                <button onclick="window.location.href='PurchaseConfirm.php' " class="check_nav">
+                    Check
+                </button>
+            </footer>
+        </div>
     </div>
-</div>
-<div class="notificate">
-    <div class="noti_warning">
-        <h2>Chào Bạn! Trang này chỉ dành cho người quản lý, vui lòng đăng nhập tài khoản admin!</h2>
-    </div>
+    <div class="notificate">
+        <div class="noti_warning">
+            <h2>Chào Bạn! Trang này chỉ dành cho người quản lý, vui lòng đăng nhập tài khoản admin!</h2>
+        </div>
 
-    <section class="sign_up" id="sign_up">
-        <form id="form_2" class="form" action="VerifyLogin.php" method="POST">
-            <div class="content_form">
-                <div class="form_sign">
-                    <div class="info">
-                        <label for="adminname" class="label-form">Tên đăng nhập</label>
-                        <input id="adminname" placeholder="Tên đăng nhập" type="text" class="item_form" name="adminname">
-                        <span class="form_message error" id="adminname-error"></span>
-                    </div>
-                    <div id="eyes" class="info">
-                        <label for="password" class="label-form">Mật Khẩu</label>
-                        <input id="password" placeholder="Vui lòng nhập mật khẩu" type="password" class="item_form" name="password">
-                        <span class="form_message error" id="password-error"></span>
-                        <div class="eye-icon" onclick="togglePassword()">
-                            <i id="eyeIcon" class="fa-solid fa-eye-slash"></i>
+        <section class="sign_up" id="sign_up">
+            <form id="form_2" class="form" action="VerifyLogin.php" method="POST">
+                <div class="content_form">
+                    <div class="form_sign">
+                        <div class="info">
+                            <label for="adminname" class="label-form">Tên đăng nhập</label>
+                            <input id="adminname" placeholder="Tên đăng nhập" type="text" class="item_form"
+                                name="adminname">
+                            <span class="form_message error" id="adminname-error"></span>
                         </div>
-                    </div>
-                    <div class="submit_form">
-                        <!--
-                        <button class="done" type="button" id="done" name="login">Đăng Nhập</button>
-                        <div id="error-container" class="error-container">
-                            <span id="error-message" class="error-message"></span>
-                        -->
-                        <!--chỉnh lại logic button-->
-                        <button name="btn-login">Đăng nhập</button>
+                        <div id="eyes" class="info">
+                            <label for="password" class="label-form">Mật Khẩu</label>
+                            <input id="password" placeholder="Vui lòng nhập mật khẩu" type="password" class="item_form"
+                                name="password">
+                            <span class="form_message error" id="password-error"></span>
+                            <div class="eye-icon" onclick="togglePassword()">
+                                <i id="eyeIcon" class="fa-solid fa-eye-slash"></i>
+                            </div>
+                        </div>
+                        <div class="submit_form">
+
+                            <button class="done" id="done" name="btn-login">Đăng nhập</button>
+                            <div id="error-container" class="error-container">
+                                <span id="error-message" class="error-message"></span>
+
+                                <!--chỉnh lại logic button-->
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </form>
-    </section>
-</div>
-
-
-<footer class="foooter_page1">
-    <div class="lg_end">
-        <a href="./Concert_B.php">
-            <img id="lg_ft" src="../img/logo_page.png" alt=""></a>
-
+            </form>
+        </section>
     </div>
-</footer>
+
+
+    <footer class="foooter_page1">
+        <div class="lg_end">
+            <a href="./Concert_B.php">
+                <img id="lg_ft" src="../img/logo_page.png" alt=""></a>
+
+        </div>
+    </footer>
 
 </body>
 <script src="../js/form_test.js"></script>
